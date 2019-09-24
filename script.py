533e45f6c588ae0c680dcbfb400158602504114d
KChristelle <r.ayateke@alustudent.com>
2019-09-24 13:07:06 +0200

    print out question

---
+++ script.py
@@ -0,0 +1,17 @@
+print('Hello there, let us play hangman!!')
+print('-----------------------------------')
+print('You have a total score of six. Every time you get an answer wrong you loose a point.')
+print('-----------------------------------')
+score = 6
+
+first_quest = input('When was ALU founded?')  # 2015
+
+second_quest = input('Who is the CEO of ALU?')  # Fred Swaniker
+third_quest = input('Where are ALU campuses?')   # Rwanda and Mauritius
+fourth_quest = input('How many campuses does ALU have?')  # 2
+fifth_quest = input('What is the name of ALU Rwanda’s Dean?')   # Dr Gaidi Faraj
+sixth_quest = input('Who is in Charge of Student Life?')  # Sila
+seventh_quest = input('What is the name of our Lab?')  # Nigeria
+eight_quest = input('How many students do we have in year 2 Cs?')
+ninth_quest = input('How many degrees does ALU offer?')  # 4
+tenth_quest = input('Where are the headquarters of ALU?')  # Mauritius
